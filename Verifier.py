3bb0b385b36e26968376bf72cb70a80dcfb843f1
Erin Strain <erinsrandomstuffs@gmail.com>
2020-10-05 18:51:57 -0600

    Finished reading in files. Began tests to verify the output

--- Verifier.py
+++ Verifier.py
@@ -18,20 +18,46 @@ def readFiles(inputFileName, outputFileName):
         
         iDictionaryKeys = list(range(1, len(iDictionaryValues)+1))
         
-        iDictionaryOfPoints = {} 
+        iDictionaryOfPoints = dict(zip(iDictionaryKeys, iDictionaryValues))
+
+        #read in the list of output sets
+        oListSets = oFile.readlines()
+        
+        #pop the maxlength
+        maxDistance = oListSets[0]
+        oListSets.pop(0)
         
         
-        
-        
-        
-        
-        
-        
-        
-        return True
+        return iDictionaryOfPoints, oListSets, numPoints, numSets, maxDistance
 
 
-
+def allTests(iDictionaryOfPoints, oListSets, numPoints, numSets, maxDistance):
+        
+        testsPass = True
+        numSortedPoints = 0
+        
+        #compare input k with the number of sets
+        if (numSets != len(oListSets)):
+            print("Incorrect number of sets")
+            testPass = False
+            
+        #ensure that the sets are not empty
+        for sets in oListSets:
+            if len(sets) == 0:
+                print("There is a non-empty set")
+                testPass = False
+            numSortedPoints += len(sets)
+            
+        
+        #compare the input n with number of points
+        
+        
+        
+        
+        
+        
+        
+        
 
 
 
@@ -44,7 +70,8 @@ def main():
     testInput = "exampleinput.txt"
     testOutput = "exampleoutput.txt"
     
-    verifier = readFiles(testInput, testOutput)
+    fileSpecs = readFiles(testInput, testOutput)
+    verifier = allTests(fileSpecs[0], fileSpecs[1], fileSpecs[2], fileSpecs[3], fileSpecs[4])
     print(verifier)
 
 if __name__ == "__main__":   #Calls main
