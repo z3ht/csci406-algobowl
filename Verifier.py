43190be0a2d07570206d38df41ca386b32c7a56b
Erin Strain <erinsrandomstuffs@gmail.com>
2020-10-06 23:11:21 -0600

    Completed a possible solution to verify output

--- Verifier.py
+++ Verifier.py
@@ -4,28 +4,45 @@ Created on Mon Oct  5 17:30:02 2020
 
 @author: Frankie Austin
 """
+import time
 
+start_time = time.time()
 
 def readFiles(inputFileName, outputFileName): 
         iFile = open(inputFileName, "r")
         oFile = open(outputFileName, "r")
         
         iDictionaryValues = iFile.readlines()
-        numPoints = iDictionaryValues[0]  #I initially assigned the weight and the number of unique items to a value of 0. This way, I can pop them out and store them in the following variables. 
-        numSets = iDictionaryValues[1] #Weight capacity
+        numPoints = int(iDictionaryValues[0])  #I initially assigned the weight and the number of unique items to a value of 0. This way, I can pop them out and store them in the following variables. 
+        numSets = int(iDictionaryValues[1]) #Weight capacity
         iDictionaryValues.pop(0)
         iDictionaryValues.pop(0)
-        
+        i_point_values = []
+        temp_list = []
+        array_of_point_values = []
+        for value in iDictionaryValues:
+            temp_list = value.split()
+            for point in range(len(temp_list)):
+                temp_list[point] = int(temp_list[point])
+            i_point_values.append(temp_list)
+            
         iDictionaryKeys = list(range(1, len(iDictionaryValues)+1))
         
-        iDictionaryOfPoints = dict(zip(iDictionaryKeys, iDictionaryValues))
+        iDictionaryOfPoints = dict(zip(iDictionaryKeys, i_point_values))
 
         #read in the list of output sets
-        oListSets = oFile.readlines()
-        
+        o_sets = oFile.readlines()
         #pop the maxlength
-        maxDistance = oListSets[0]
-        oListSets.pop(0)
+        maxDistance = int(o_sets[0])
+        o_sets.pop(0)        
+        oListSets = []   
+        for value in o_sets:
+            temp_list = value.split()
+            for point in range(len(temp_list)):
+                temp_list[point] = int(temp_list[point])
+            oListSets.append(temp_list)
+        
+        
         
         
         return iDictionaryOfPoints, oListSets, numPoints, numSets, maxDistance
@@ -39,33 +56,42 @@ def allTests(iDictionaryOfPoints, oListSets, numPoints, numSets, maxDistance):
         #compare input k with the number of sets
         if (numSets != len(oListSets)):
             print("Incorrect number of sets")
-            testPass = False
+            testsPass = False
             
         #ensure that the sets are not empty
         for sets in oListSets:
             if len(sets) == 0:
                 print("There is a non-empty set")
-                testPass = False
+                testsPass = False
             numSortedPoints += len(sets)
         
         #compare the input n with number of points
-        if numSets != numSortedPoints:
+        if numPoints != numSortedPoints:
             print("There are an incorrect number of points sorted")
-            testPass = False
+            testsPass = False
             
         #verify max distance is correct
+        calculated_max_distance = 0
+        max_distance_set = 0
         for sets in oListSets:
-            key = 0
-            for point in sets:
+            for a in sets:
+                point1 = iDictionaryOfPoints[int(a)]
+                for b in sets:
+                    point2 = iDictionaryOfPoints[int(b)]
+                    x_distance = point1[0] - point2[0]
+                    y_distance = point1[1] - point2[1]
+                    z_distance = point1[2] - point2[2]
+                    total_distance = abs(x_distance) + abs(y_distance) + abs(z_distance)
+                    if calculated_max_distance < total_distance:
+                        calculated_max_distance = total_distance
+                        max_distance_set = sets
+                        
+        if maxDistance != calculated_max_distance:
+            print("The max distance was incorrect for the following set")
+            print(max_distance_set)
+            testsPass = False
                 
-    
-        
-        
-        
-        
-        
-        
-
+        return testsPass
 
 
 def ourAlgorithm():
@@ -79,6 +105,7 @@ def main():
     
     fileSpecs = readFiles(testInput, testOutput)
     verifier = allTests(fileSpecs[0], fileSpecs[1], fileSpecs[2], fileSpecs[3], fileSpecs[4])
+    print(format((time.time() - start_time), '.014f'))
     print(verifier)
 
 if __name__ == "__main__":   #Calls main
